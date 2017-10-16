230b1bef61fc3cb3d7992680e805ccdc2e4d601a
DESKTOP-08J2J6D\박준호 <wnsgh8879@naver.com>
2017-10-16 21:13:03 +0900

    commit

---
+++ src/madvirus/spring/chap06/service/ArticleService.java
@@ -0,0 +1,8 @@
+package madvirus.spring.chap06.service;
+
+public class ArticleService {
+
+	public void writeArticle(NewArticleCommand command) {
+		System.out.println("신규 게시글 등록 : " + command);
+	}
+}
